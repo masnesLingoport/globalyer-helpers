4f962ed460eaefb069d7d47618e6f4856882a772
Michael Asnes <mike.asnes@gmail.com>
2016-02-22 20:25:09 -0700

    Initial working build

--- find-common.py
+++ find-common.py
@@ -1,5 +1,6 @@
 import csv
 import re
+import string
 import sys
 """
 Print out all lines containing a similar word.
@@ -17,6 +18,8 @@ Output: ELGS Truncated filename:line#, issue
 """
 
 FILENAME_LENGTH = 30
+PUNCTUATION_SET = set(string.punctuation)
+MIN_SIMILAR_DEFAULT = 2
 
 
 class WordInfo(object):
@@ -26,7 +29,11 @@ class WordInfo(object):
         self.line_numbers = {}  # {filename: line_number_unsorted}
         self.issue_map = {}  # filename + str(line_number)
 
+    def __len__(self):
+        return len(self.issue_map)
+
     def add(self, filename, line_number, issue):
+        line_number = int(line_number)
         self._add_filename(filename)
         self._add_line_number(filename, line_number)
         issue_key = self._issue_key(filename, line_number)
@@ -43,24 +50,30 @@ class WordInfo(object):
         if filename not in self.line_numbers:
             self.line_numbers[filename] = [line_number]
         else:
-            self.line_numbers[filename].append(line_number)
+            line_number_list = self.line_numbers[filename]
+            if len(line_number_list) > 0 and line_number_list[-1] != line_number:
+                line_number_list.append(line_number)
 
     def _add_or_merge_issue(self, issue_key, issue):
         if issue_key in self.issue_map:
+            if self.issue_map[issue_key].get_issue_types() != issue.get_issue_types():
+                print(self.issue_map[issue_key].get_issue_types())
+                print(issue.get_issue_types())
+                exit()
             twin = self.issue_map[issue_key]
-            for issue_type in issue.get_issue_types():
+            for issue_type in issue.get_issue_types().replace(' ', ''):
                 twin.add_issue_type(issue_type)
         else:
             self.issue_map[issue_key] = issue
 
     def getIterator(self):
-        return self._WordIterator(self.word, self.filenames, self.line_numbers)
+        return self._WordIterator(self.word, self.filenames, self.line_numbers, self.issue_map)
 
     class _WordIterator(object):
         def __init__(self, word, filenames, line_numbers, issue_map):
             self.word = word
             self.filenames = sorted(filenames)
-            self.line_numbers = sorted(line_numbers)
+            self.line_numbers = line_numbers
             self.issue_map = issue_map
 
         def __iter__(self):
@@ -82,7 +95,7 @@ class Issue(object):
         self._issue_types = set(issue_type)
 
     def add_issue_type(self, issue_type):
-        assert issue_type in ('E', 'L', 'G', 'S')
+        assert issue_type in ('E', 'L', 'G', 'S'), issue_type
         self._issue_types.add(issue_type)
 
     def get_issue_types(self):
@@ -98,7 +111,7 @@ class csvWrapper(object):
 
     def getReader(self):
         csvReader = csv.reader(self.csvFile)
-        csvReader.next()  # Discard starting row
+        csvReader.__next__()  # Discard starting row
         return csvReader
 
 
@@ -108,48 +121,58 @@ class Translator(object):
         return issue_type[0]
 
 
+def remove_punctuation(word):
+    return ''.join(ch for ch in word if ch not in PUNCTUATION_SET)
+
+
 def readCsvFile(csvFile):
     words = {}  # {word: WordInfo}
-    for row in csvWrapper(csvFile).getReader():
-        priority, file, line_num, issue_type, issue, code_line = row
-        for word in re.split('\s+', code_line):
-            issue = Issue(code_line, Translator.getIssueLetter(issue_type))
-            if word in words:
-                wordInfo = words[word]
-            else:
-                wordInfo = WordInfo(word)
+    with open(csvFile) as f:
+        reader = csv.reader(f)
+        reader.__next__()  # Discard starting row
+        for priority, file, line_num, issue_type, issue, code_line in reader:
+            # if re.match("[^\w\"'\(\)\[\]\{\}]+$", word):  # Only Punctuation, no brackets/'"
+            for word in re.split("[^\w]", code_line):
+                word = remove_punctuation(word)
+                if len(word) < 2:
+                    continue
+                issue = Issue(code_line, Translator.getIssueLetter(issue_type))
+                wordInfo = words[word] if word in words else WordInfo(word)
                 words[word] = wordInfo
-            wordInfo.add(file, line_num, issue)
+                wordInfo.add(file, line_num, issue)
     return words
 
 
-def printInfo(object):
-    def __init__(self, wordInfo):
-        self.wordInfo = wordInfo
-        _print()
+def setToLength(string, length):
+    string += ' ' * max(0, length - len(string))
+    return string[-length:]
 
-    def _setToLength(self, string, length):
-        string += ' ' * max(0, length - len(string))
-        return string[:-length]
 
-    def _print(self):
-        for issue_types, filename, line_num, line_content in self.wordInfo.getIterator():
-            filename = self._setToLength(filename, FILENAME_LENGTH)
-            print(issue_types, filename+":"+line_num, line_content)
+def print_(wordInfo, min_similar_issues):
+    if len(wordInfo) < min_similar_issues:
+        return
+    it = wordInfo.getIterator()
+    it = it.__next__()
+    print()
+    print(wordInfo.word + ":")
+    for issue_types, filename, line_num, line_content in it:
+        filename = setToLength(filename, FILENAME_LENGTH)
+        print(issue_types, filename+":"+str(line_num), line_content)
 
 
 def main():
-    if len(sys.argv) != 2:
+    if 2 > len(sys.argv) or len(sys.argv) > 3:
         usage()
         exit()
     csvFile = sys.argv[1]
+    min_similar_issues = int(sys.argv[2] if len(sys.argv) == 3 else MIN_SIMILAR_DEFAULT)
     wordInfos = readCsvFile(csvFile)
-    for wordInfo in sorted(wordInfos):
-        printInfo(wordInfo)
-
+    for word in sorted(wordInfos):
+        print_(wordInfos[word], min_similar_issues)
 
 def usage():
-    print("python", sys.argv[0], "pathToCsvFile/csvFile.csv")
+    print("Usage:")
+    print("python", sys.argv[0], "pathToCsvFile/csvFile.csv", "[min similar issues to display]")
 
 if __name__ == '__main__':
     main()
